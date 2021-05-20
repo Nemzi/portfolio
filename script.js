c1fb2dcd5feb36b06c7bbd602938c70fe9c1f5f9
Nemanja Simic <nemanja.milan.simic@gmail.com>
2021-05-21 00:03:40 +0200

    Formulated all pages, technologies module finished, all responsive, starting js implementantation

--- script.js
+++ script.js
@@ -1,25 +1,67 @@
+let noDisplay = document.getElementById("swapiTable");
+noDisplay.style.display = "none";
+
+// can create delay when used need to make a more optimized version of the fetch
+
+function getSelectedValue() {
+    let selectedElement = document.getElementById("starWarsSelectForm").value;
+    noDisplay.style.display = "flex";
+
+    if (selectedElement == 1) {
+        fetchData('https://swapi.dev/api/people/1');
+        showPicture("images/swapi/swapiLukeSkywalker.png");
+    } else if (selectedElement == 2) {
+        fetchData('https://swapi.dev/api/people/4');
+        showPicture("images/swapi/swapiDarthVader.jpeg");
+    } else if (selectedElement == 3) {
+        fetchData('https://swapi.dev/api/people/20');
+        showPicture("images/swapi/swapiYoda.png");
+    } else if (selectedElement == 4) {
+        fetchData('https://swapi.dev/api/people/10');
+        showPicture("images/swapi/swapiObiVanKenobi.png");
+    } else if (selectedElement == 5) {
+        fetchData('https://swapi.dev/api/people/21');
+        showPicture("images/swapi/swapiSheevPalpatine.png")
+    }
     
-            
-            
-            
-            
-            const url = 'https://swapi.dev/api/people/1';
+}
 
-            fetch(url)
-                .then(function (response) {
-                    return response.json();
-                })
-                .then(function (data) {
-                    appendData(data);
-                })
-                .catch(function (err) {
-                    console.log('error: ' + err);
-                });
+function fetchData(url) {
+    fetch(url)
+        .then(function (response) {
+            return response.json();
+            })
+        .then(function (data) {
+            displayData(data);
+            })
+        .catch(function (err) {
+            console.log('error: ' + err);
+            });
+}
+            
+function displayData(data) {
+    let name = document.getElementById("swapiName");
+    let height = document.getElementById("swapiHeight");
+    let hairColor = document.getElementById("swapiHairColor");
+    let skinColor = document.getElementById("swapiSkinColor");
+    let eyeColor = document.getElementById("swapiEyeColor");
+    let birthYear = document.getElementById("swapiBirthYear");
+    let gender = document.getElementById("swapiGender");
+    
+    name.innerHTML = data.name;
+    height.innerHTML = data.height;
+    hairColor.innerHTML = data.hair_color;
+    skinColor.innerHTML = data.skin_color;
+    eyeColor.innerHTML = data.eye_color;
+    birthYear.innerHTML = data.birth_year;
+    gender.innerHTML = data.gender;
+}
 
-            function appendData(data) {
-                var mainContainer = document.getElementById("myData");
-                var div = document.createElement("div");
-                div.innerHTML = "Name : " + data.name;
-                mainContainer.appendChild(div);
-                
-            }
+function showPicture(source) {
+    let img = document.getElementById("swapiPicture");
+    img.src = source;
+    img.style.display = "block";
+    img.style.width = "100%";
+    
+    
+}
